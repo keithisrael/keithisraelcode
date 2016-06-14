e14ce0c9445d3cbd8cda35036bb63a9d85d1c28f
keithisrael <keith.israel.m@gmail.com>
2016-06-14 15:21:54 -0500

    Initial Tests
    Tests 1,5,10,50, 100,500,1000

---
+++ RomanConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanConverter
+{
+    public class RomanConverter
+    {
+        Dictionary<int, string> romanTable = null;
+
+        public RomanConverter()
+        {
+            romanTable = new Dictionary<int, string>();
+            romanTable.Add(1000, "M");
+            romanTable.Add(500, "D");
+            romanTable.Add(100, "C");
+            romanTable.Add(50, "L");
+            romanTable.Add(10, "X");
+            romanTable.Add(5, "V");
+            romanTable.Add(1, "I");
+        }
+
+        public string convert(int inputNumber)
+        {
+            return romanTable[inputNumber];
+        }
+    }
+}
---
+++ RomanConverterTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanConverter.Test
+{
+    [TestClass]
+    public class RomanConverterTest
+    {
+        RomanConverter rcon = new RomanConverter();
+
+        /**********************************************************************************************
+         
+            THE INITIAL SETUP TESTS
+         
+         **********************************************************************************************/
+
+
+        [TestMethod]
+        public void TestReturnIWhenpassed1()
+        {
+            string romanValue = rcon.convert(1);
+            Assert.AreEqual("I", romanValue, String.Format("Expected: I \n Returned: {0}", romanValue));
+        }
+
+        [TestMethod]
+        public void TestReturnVWhenpassed5()
+        {
+            string romanValue = rcon.convert(5);
+            Assert.AreEqual("V", romanValue, String.Format("Expected: V \n Returned: {0}", romanValue));
+        }
+
+        [TestMethod]
+        public void TestReturnXWhenpassed10()
+        {
+            string romanValue = rcon.convert(10);
+            Assert.AreEqual("X", romanValue, String.Format("Expected: X \n Returned: {0}", romanValue));
+        }
+
+        [TestMethod]
+        public void TestReturnLWhenpassed50()
+        {
+            string romanValue = rcon.convert(50);
+            Assert.AreEqual("L", romanValue, String.Format("Expected: L \n Returned: {0}", romanValue));
+        }
+
+        [TestMethod]
+        public void TestReturnCWhenpassed100()
+        {
+            string romanValue = rcon.convert(100);
+            Assert.AreEqual("C", romanValue, String.Format("Expected: C \n Returned: {0}", romanValue));
+        }
+
+        [TestMethod]
+        public void TestReturnDWhenpassed500()
+        {
+            string romanValue = rcon.convert(500);
+            Assert.AreEqual("D", romanValue, String.Format("Expected: D \n Returned: {0}", romanValue));
+        }
+
+        [TestMethod]
+        public void TestReturnMWhenpassed1000()
+        {
+            string romanValue = rcon.convert(1000);
+            Assert.AreEqual("M", romanValue, String.Format("Expected: M \n Returned: {0}", romanValue));
+        }
+
+    }
+}
